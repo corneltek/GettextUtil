280f76682ea01c8bc0d702f53def8420134390ef
c9s <cornelius.howl@gmail.com>
2011-12-04 21:07:52 +0800

    Add Parser class.

---
+++ src/GettextUtil/GettextParser.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace GettextUtil;
+
+class GettextParser 
+{
+
+
+}
+
